1cf3e4c2b3f7a2742703cf90ebaa4a1bf4b643c4
JCGM <juankarlos27@hotmail.es>
2017-05-31 21:39:25 -0500

    cambio

--- js/ocultar.js
+++ js/ocultar.js
@@ -1,21 +1,28 @@
 
 function mostrargit() {
             document.getElementById('GIT').style.display='block';
+            document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
             document.getElementById('html').style.display='none';
             document.getElementById('css3').style.display='none';
             document.getElementById('html_pdf').style.display='none';
             document.getElementById('html_css3').style.display='none';
+            document.getElementById('que-es').style.display='none';
             document.getElementById('tutorial1').style.display='none';
             document.getElementById('tutorial4').style.display='none';
             document.getElementById('tutorial3').style.display='none';
             document.getElementById('tutorial2').style.display='none';
             document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
 
         }
 
 function mostrarhtml() {
             document.getElementById('html').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
             document.getElementById('html_pdf').style.display='block';
             document.getElementById('GIT').style.display='none';
@@ -26,11 +33,17 @@ function mostrarhtml() {
             document.getElementById('tutorial3').style.display='none';
             document.getElementById('tutorial2').style.display='none';
             document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
+            document.getElementById('que-es').style.display='none';
 
         }
 
 function mostrarcss3() {
             document.getElementById('css3').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('html_css3').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
             document.getElementById('html').style.display='none';
@@ -41,79 +54,208 @@ function mostrarcss3() {
             document.getElementById('tutorial3').style.display='none';
             document.getElementById('tutorial2').style.display='none';
             document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
+            document.getElementById('que-es').style.display='none';
         }
 
 function mostrarvideo1() {
             document.getElementById('tutorial1').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
             document.getElementById('css3').style.display='none';
             document.getElementById('html_css3').style.display='none';
             document.getElementById('html').style.display='none';
             document.getElementById('GIT').style.display='none';
             document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
         }
 
 function mostrarvideo2() {
             document.getElementById('tutorial2').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
-            document.getElementById('tutorial1').style.display='none';
             document.getElementById('css3').style.display='none';
             document.getElementById('html_css3').style.display='none';
             document.getElementById('html').style.display='none';
             document.getElementById('GIT').style.display='none';
             document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
+
         }
 
 function mostrarvideo3() {
             document.getElementById('tutorial3').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
-            document.getElementById('tutorial2').style.display='none';
-            document.getElementById('tutorial1').style.display='none';
             document.getElementById('css3').style.display='none';
             document.getElementById('html_css3').style.display='none';
             document.getElementById('html').style.display='none';
             document.getElementById('GIT').style.display='none';
             document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
         }
 
 function mostrarvideo4() {
             document.getElementById('tutorial4').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
-            document.getElementById('tutorial3').style.display='none';
-            document.getElementById('tutorial2').style.display='none';
-            document.getElementById('tutorial1').style.display='none';
             document.getElementById('css3').style.display='none';
-            document.getElementById('html_css3').style.display='none';
             document.getElementById('html').style.display='none';
             document.getElementById('GIT').style.display='none';
             document.getElementById('html_pdf').style.display='none';
-        }
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';        }
 
 function mostrarvideo5() {
             document.getElementById('tutorial5').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
-            document.getElementById('tutorial4').style.display='none';
-            document.getElementById('tutorial3').style.display='none';
-            document.getElementById('tutorial2').style.display='none';
-            document.getElementById('tutorial1').style.display='none';
             document.getElementById('css3').style.display='none';
             document.getElementById('html_css3').style.display='none';
             document.getElementById('html').style.display='none';
             document.getElementById('GIT').style.display='none';
             document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
         }
 
 function mostrarvideo6() {
             document.getElementById('tutorial6').style.display='block';
+             document.getElementById('contenedor').style.display='block';
             document.getElementById('clearFixGIT').style.display='block';
-            document.getElementById('tutorial5').style.display='none';
-            document.getElementById('tutorial4').style.display='none';
-            document.getElementById('tutorial3').style.display='none';
-            document.getElementById('tutorial2').style.display='none';
-            document.getElementById('tutorial1').style.display='none';
             document.getElementById('css3').style.display='none';
             document.getElementById('html_css3').style.display='none';
             document.getElementById('html').style.display='none';
             document.getElementById('GIT').style.display='none';
             document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
+        }
+
+
+function mostrarvideo7() {
+            document.getElementById('tutorial7').style.display='block';
+             document.getElementById('contenedor').style.display='block';
+            document.getElementById('clearFixGIT').style.display='block';
+            document.getElementById('css3').style.display='none';
+            document.getElementById('html_css3').style.display='none';
+            document.getElementById('html').style.display='none';
+            document.getElementById('GIT').style.display='none';
+            document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
+        }
+
+function mostrarvideo8() {
+            document.getElementById('tutorial8').style.display='block';
+             document.getElementById('contenedor').style.display='block';
+            document.getElementById('clearFixGIT').style.display='block';
+            document.getElementById('css3').style.display='none';
+            document.getElementById('html_css3').style.display='none';
+            document.getElementById('html').style.display='none';
+            document.getElementById('GIT').style.display='none';
+            document.getElementById('html_pdf').style.display='none';
+             document.getElementById('que-es').style.display='none';
+             document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
+        }
+
+function mostrarvideo9() {
+            document.getElementById('tutorial9').style.display='block';
+            document.getElementById('contenedor').style.display='block';
+            document.getElementById('clearFixGIT').style.display='block';
+            document.getElementById('css3').style.display='none';
+            document.getElementById('html_css3').style.display='none';
+            document.getElementById('html').style.display='none';
+            document.getElementById('GIT').style.display='none';
+            document.getElementById('html_pdf').style.display='none';
+            document.getElementById('que-es').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial').style.display='none';
+        }
+
+function mostrarque() {
+            document.getElementById('que-es').style.display='block';
+            document.getElementById('contenedor').style.display='block';
+            document.getElementById('clearFixGIT').style.display='block';
+            document.getElementById('css3').style.display='none';
+            document.getElementById('html_css3').style.display='none';
+            document.getElementById('html').style.display='none';
+            document.getElementById('GIT').style.display='none';
+            document.getElementById('tutorial1').style.display='none';
+            document.getElementById('tutorial2').style.display='none';
+            document.getElementById('tutorial3').style.display='none';
+            document.getElementById('tutorial4').style.display='none';
+            document.getElementById('tutorial5').style.display='none';
+            document.getElementById('tutorial6').style.display='none';
+            document.getElementById('tutorial7').style.display='none';
+            document.getElementById('tutorial8').style.display='none';
+            document.getElementById('tutorial9').style.display='none';
         }
